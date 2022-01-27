a50f05f088fdb1686689094a00174b3fed609450
junsantilla <junsantilla@live.com>
2022-01-27 18:08:17 +0800

    Add functions: Add task, Delete task, Delete all

---
+++ app.js
@@ -0,0 +1,49 @@
+const taskInput = document.querySelector('.task-input');
+const addBtn = document.querySelector('.add-task');
+const taskList = document.querySelector('.task-list');
+const clearTaskListBtn = document.querySelector('.clear-task-list');
+
+function loadAllEveentHandlers() {
+    console.log("Event handler loaded");
+    addBtn.addEventListener("click", addList);
+    taskInput.addEventListener("keypress", function (e) {
+        if (e.key === 'Enter') {
+            addList();
+        }
+    });
+    taskList.addEventListener("click", removeTask);
+    clearTaskListBtn.addEventListener("click", clearTaskList);
+}
+
+loadAllEveentHandlers();
+
+// Add task to the list
+function addList(e) {
+    if (taskInput.value == ""){
+        alert("Please add a task!")
+    } else {
+        const task = document.createElement("li");
+        task.classList = "list-group-item d-flex justify-content-between align-items-center";
+        task.appendChild(document.createTextNode(taskInput.value));
+        taskList.appendChild(task);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList = "btn-close";
+        task.appendChild(deleteBtn);
+    
+        taskInput.value = "";
+    }
+    e.preventDefault();
+}
+
+// Delete all tasks
+function clearTaskList(e) {
+    taskList.innerHTML = "";
+}
+
+// Remove task from the list
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains("list-group-item")) {
+        e.target.parentElement.remove();
+    }
+}
